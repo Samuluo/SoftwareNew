6d92ce3465e756bbdbf252cc9b777b85478967f8
hjh <1556332597@qq.com>
2022-03-01 21:21:18 +0800

    图片上传功能

--- devicedemo后端分层模型参考-源码/MealOrderSys/src/main/java/com/example/demo/web/controller/FileController.java
+++ devicedemo后端分层模型参考-源码/MealOrderSys/src/main/java/com/example/demo/web/controller/FileController.java
@@ -1,23 +1,50 @@
 package com.example.demo.web.controller;
 
-import com.example.demo.common.JsonResponse;
-import com.example.demo.service.SysSubjectService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.demo.service.FileService;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/file")
+@Slf4j
 public class FileController {
-    @Autowired
-    private SysSubjectService sysSubjectService;
-    /**
-     * 文件上传
-     */
-    @PostMapping(value = "/uploadFile")
-    @ResponseBody
-    public JsonResponse uploadFile(@RequestParam("file") MultipartFile multipartFile) throws Exception {
-        sysSubjectService.uploadSubjectMould(multipartFile);
-        return JsonResponse.success("上传完成");
+    protected FileService fileService;
+
+    protected ResourceLoader resourceLoader;
+
+    public FileController(FileService fileService, ResourceLoader resourceLoader) {
+        this.fileService = fileService;
+        this.resourceLoader = resourceLoader;
     }
+
+    @ApiOperation(value = "文件上传", notes = "文件上传")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public ResponseEntity<Map<String, String>> upload(MultipartFile file, HttpServletRequest request) throws IOException {
+        Map<String, String> map = new HashMap();
+        /*if (SecurityUtils.getCurrentUserInfo() == null) {
+            map.put("message", "请登录后重试");
+            return ResponseEntity.badRequest().body(map);
+        }*/
+        map = fileService.upload(file);
+        return ResponseEntity.ok().body(map);
+    }
+
+    private static String suffix(String fileName) {
+        int i = fileName.lastIndexOf('.');
+        return i == -1 ? "" : fileName.substring(i + 1);
+    }
+
 }
